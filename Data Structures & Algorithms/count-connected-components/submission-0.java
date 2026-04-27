1e8ee191771badaea1abf8553db9b22dc3f67bf4
Cao Minh Nguyen <minhnc2005@gmail.com>
2026-04-27 19:25:56 +0200

    Add: count-connected-components - submission-0

---
+++ Data Structures & Algorithms/count-connected-components/submission-0.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int countComponents(int n, int[][] edges) {
+        List<List<Integer>> graph = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        for (int[] e : edges) {
+            graph.get(e[0]).add(e[1]);
+            graph.get(e[1]).add(e[0]);
+        }
+
+        int numComponents = 0;
+        boolean[] visited = new boolean[n];
+
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                numComponents++;
+                dfs(i, graph, visited);
+            }
+        }
+
+        return numComponents;
+    }
+
+    private void dfs(int node, List<List<Integer>> graph, boolean[] visited) {
+        visited[node] = true;
+
+        for (int nei : graph.get(node)) {
+            if (!visited[nei]) {
+                dfs(nei, graph, visited);
+            }
+        }
+    }
+}
