1df272e4d00c2cf2f5e3d3abb69ad42a273e32bd
Cao Minh Nguyen <minhnc2005@gmail.com>
2026-04-27 19:20:14 +0200

    Add: valid-tree - submission-5

---
+++ Data Structures & Algorithms/valid-tree/submission-5.java
@@ -0,0 +1,39 @@
+class Solution {
+    public boolean validTree(int n, int[][] edges) {
+        // A graph is a valid tree iff: number of edges = n - 1, graph is connected
+        if (edges.length != n - 1) {
+            return false;
+        }
+
+        List<List<Integer>> adjList = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            adjList.add(new ArrayList<>());
+        }
+
+        for (int[] e : edges) {
+            adjList.get(e[0]).add(e[1]);
+            adjList.get(e[1]).add(e[0]);
+        }
+
+        boolean[] visited = new boolean[n];
+        dfs(0, adjList, visited);
+
+        for (boolean val : visited) {
+            if (!val) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private void dfs(int node, List<List<Integer>> adjList, boolean[] visited) {
+        visited[node] = true;
+
+        for (int nei : adjList.get(node)) {
+            if (!visited[nei]) {
+                dfs(nei, adjList, visited);
+            }
+        }
+    }
+}
