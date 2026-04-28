8c0d62ee36abfefd76ae25ef2d8d7db200f5df11
Cao Minh Nguyen <minhnc2005@gmail.com>
2026-04-28 17:24:23 +0200

    Add: reconstruct-flight-path - submission-1

---
+++ Data Structures & Algorithms/reconstruct-flight-path/submission-1.java
@@ -0,0 +1,34 @@
+class Solution {
+    public List<String> findItinerary(List<List<String>> tickets) {
+        // Iterate through E tickets. For each ticket, insert into PQ takes O(log E)
+        Map<String, PriorityQueue<String>> graph = new HashMap<>();
+        for (List<String> pair : tickets) {
+            String from = pair.get(0);
+            String to = pair.get(1);
+            graph.putIfAbsent(from, new PriorityQueue<>());
+            graph.get(from).offer(to);
+        }
+
+        List<String> res = new ArrayList<>();
+
+        // DFS visits each edge once, each poll takes O(log E) -> Total O(E log E)
+        dfs("JFK", graph, res);
+        Collections.reverse(res);
+
+        return res;
+    }
+
+    private void dfs(String node, Map<String, PriorityQueue<String>> graph, List<String> res) {
+        PriorityQueue<String> neighbors = graph.get(node);
+
+        while (neighbors != null && !neighbors.isEmpty()) {
+            String nei = neighbors.poll();
+            dfs(nei, graph, res);
+        }
+
+        res.add(node);
+    }
+
+    // Time: O(E log E)
+    // Space: O(V + E)
+}
