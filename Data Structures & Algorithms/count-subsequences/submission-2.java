992e3e246fdc94c87bde59e675b159b003c0ad9e
Cao Minh Nguyen <minhnc2005@gmail.com>
2026-05-04 10:54:34 +0200

    Add: count-subsequences - submission-2

---
+++ Data Structures & Algorithms/count-subsequences/submission-2.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int numDistinct(String s, String t) {
+        int m = s.length();
+        int n = t.length();
+
+        if (m < n) {
+            return 0;
+        }
+
+        int[][] dp = new int[m + 1][n + 1];
+
+        // Base case: empty string t can be formed from any prefix of s in 1 way
+        for (int i = 0; i <= m; i++) {
+            dp[i][0] = 1;
+        }
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                dp[i][j] = dp[i-1][j];
+
+                if (s.charAt(i-1) == t.charAt(j-1)) {
+                    dp[i][j] += dp[i-1][j-1];
+                }
+            }
+        }
+
+        return dp[m][n];
+    }
+}
