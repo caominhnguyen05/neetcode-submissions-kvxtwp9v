aabe5e6b9c983177035546e68d0f6733a201aa87
Cao Minh Nguyen <minhnc2005@gmail.com>
2026-06-11 20:24:20 +0200

    Add: target-sum - submission-7

---
+++ Data Structures & Algorithms/target-sum/submission-7.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int findTargetSumWays(int[] nums, int target) {
+        int n = nums.length;
+        int totalSum = 0;
+
+        for (int num : nums) {
+            totalSum += num;
+        }
+
+        if (target > totalSum || target < -totalSum) return 0;
+        if ((target + totalSum) % 2 == 1) {
+            return 0;
+        }
+
+        int positiveSum = (target + totalSum) / 2;
+
+        // dp[j] = number of ways to form sum j
+        int[] dp = new int[positiveSum + 1];
+
+        // base case: one way to form sum 0
+        dp[0] = 1;
+
+        for (int num : nums) {
+            for (int j = positiveSum; j >= num; j--) {
+                dp[j] += dp[j - num];
+            }
+        }
+
+        return dp[positiveSum];
+    }
+}
