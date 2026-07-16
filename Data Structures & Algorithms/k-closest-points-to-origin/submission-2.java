656f969c126cfcf5b25e689f27ee004498e94b19
Cao Minh Nguyen <minhnc2005@gmail.com>
2026-07-16 10:04:27 +0200

    Add: k-closest-points-to-origin - submission-2

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-2.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[][] kClosest(int[][] points, int k) {
+        PriorityQueue<int[]> maxHeap = new PriorityQueue<>((a, b) -> distance(b) - distance(a));
+
+        for (int[] point : points) {
+            maxHeap.add(point);
+            if (maxHeap.size() > k) {
+                maxHeap.poll();
+            }
+        }
+
+        int[][] res = new int[k][2];
+        int idx = 0;
+
+        while (!maxHeap.isEmpty()) {
+            res[idx] = maxHeap.poll();
+            idx++;
+        }
+
+        return res;
+    }
+
+    private int distance(int[] a) {
+        return a[0] * a[0] + a[1] * a[1];
+    }
+}
