eb0bcf3ed38c0033694bebb335171e4fe768c729
Cao Minh Nguyen <minhnc2005@gmail.com>
2026-07-19 19:26:27 +0200

    Add: combinations-of-a-phone-number - submission-5

---
+++ Data Structures & Algorithms/combinations-of-a-phone-number/submission-5.java
@@ -0,0 +1,30 @@
+class Solution {
+    public List<String> letterCombinations(String digits) {
+        List<String> res = new ArrayList<>();
+
+        if (digits.isEmpty()) {
+            return res;
+        }
+
+        String[] digitToChars = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+
+        backtrack(res, new StringBuilder(), 0, digits, digitToChars);
+        return res;
+    }
+
+    private void backtrack(
+        List<String> res, StringBuilder curr, int i, String digits, String[] digitToChars) {
+        if (curr.length() == digits.length()) {
+            res.add(curr.toString());
+            return;
+        }
+
+        String chars = digitToChars[digits.charAt(i) - '0'];
+
+        for (char c : chars.toCharArray()) {
+            curr.append(c);
+            backtrack(res, curr, i + 1, digits, digitToChars);
+            curr.deleteCharAt(curr.length() - 1);
+        } 
+    }
+}
