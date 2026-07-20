1a70a0734ee5161c65f881ad8acb8a02da885021
Cao Minh Nguyen <minhnc2005@gmail.com>
2026-07-20 14:27:22 +0200

    Add: implement-prefix-tree - submission-1

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-1.java
@@ -0,0 +1,56 @@
+public class TrieNode {
+    TrieNode[] children;
+    boolean isEnd;
+
+    public TrieNode() {
+        this.children = new TrieNode[26];
+        this.isEnd = false;
+    }
+}
+
+class PrefixTree {
+    private TrieNode root;
+
+    public PrefixTree() {
+        root = new TrieNode();
+    }
+
+    public void insert(String word) {
+        TrieNode curr = root;
+
+        for (char c : word.toCharArray()) {
+            if (curr.children[c - 'a'] == null) {
+                curr.children[c - 'a'] = new TrieNode();
+            }
+            curr = curr.children[c - 'a'];
+        }
+
+        curr.isEnd = true;
+    }
+
+    public boolean search(String word) {
+        TrieNode curr = root;
+
+        for (char c : word.toCharArray()) {
+            if (curr.children[c - 'a'] == null) {
+                return false;
+            }
+            curr = curr.children[c - 'a'];
+        }
+
+        return curr.isEnd;
+    }
+
+    public boolean startsWith(String prefix) {
+        TrieNode curr = root;
+
+        for (char c : prefix.toCharArray()) {
+            if (curr.children[c - 'a'] == null) {
+                return false;
+            }
+            curr = curr.children[c - 'a'];
+        }
+
+        return true;
+    }
+}
