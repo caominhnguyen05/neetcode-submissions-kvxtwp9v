596a26542ab96b3948bd57145e6166903da9deb2
Cao Minh Nguyen <minhnc2005@gmail.com>
2026-07-20 14:40:51 +0200

    Add: design-word-search-data-structure - submission-3

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-3.java
@@ -0,0 +1,58 @@
+public class TrieNode {
+    TrieNode[] children;
+    boolean isEnd;
+
+    public TrieNode() {
+        this.children = new TrieNode[26];
+        this.isEnd = false;
+    }
+}
+
+class WordDictionary {
+    private TrieNode root;
+
+    public WordDictionary() {
+        root = new TrieNode();
+    }
+
+    public void addWord(String word) {
+        TrieNode curr = root;
+
+        for (char c : word.toCharArray()) {
+            if (curr.children[c - 'a'] == null) {
+                curr.children[c - 'a'] = new TrieNode();
+            }
+
+            curr = curr.children[c - 'a'];
+        }
+
+        curr.isEnd = true;
+    }
+
+    public boolean search(String word) {
+        return dfs(word, 0, root);
+    }
+
+    private boolean dfs(String word, int startIndex, TrieNode node) {
+        TrieNode curr = node;
+        for (int i = startIndex; i < word.length(); i++) {
+            char c = word.charAt(i);
+
+            if (c == '.') {
+                for (TrieNode child : curr.children) {
+                    if (child != null && dfs(word, i + 1, child)) {
+                        return true;
+                    }
+                }
+                return false;
+            } else {
+                if (curr.children[c - 'a'] == null) {
+                    return false;
+                }
+                curr = curr.children[c - 'a'];
+            }
+        }
+
+        return curr.isEnd;
+    }
+}
