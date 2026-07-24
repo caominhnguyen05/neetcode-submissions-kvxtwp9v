a9531b0feebd23ba09d682624aa238ee0e6eaf8e
Cao Minh Nguyen <minhnc2005@gmail.com>
2026-07-24 17:32:39 +0200

    Add: course-schedule - submission-6

---
+++ Data Structures & Algorithms/course-schedule/submission-6.java
@@ -0,0 +1,50 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        List<List<Integer>> adjList = new ArrayList<>();
+        for (int i = 0; i < numCourses; i++) {
+            adjList.add(new ArrayList<>());
+        }
+
+        // Edge from course b to course a
+        for (int[] pre : prerequisites) {
+            adjList.get(pre[1]).add(pre[0]);
+        }
+
+        // 0 = unvisited, 1 = visiting, 2 = visited
+        int[] state = new int[numCourses];
+
+        for (int i = 0; i < numCourses; i++) {
+            if (state[i] == 0) {
+                if (hasCycle(i, adjList, state)) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    private boolean hasCycle(int course, List<List<Integer>> adjList, int[] state) {
+        if (state[course] == 1) {
+            return true;
+        }
+
+        if (state[course] == 2) {
+            return false;
+        }
+
+        state[course] = 1; // mark as visiting
+        for (int neighbor : adjList.get(course)) {
+            if (hasCycle(neighbor, adjList, state)) {
+                return true;
+            }
+        }
+
+        state[course] = 2;
+
+        return false;
+    }
+
+    // Time: O(V + E)
+    // Space: O(V + E)
+}
