576247afded44b4b6fadec385e467c3df1553941
Cao Minh Nguyen <minhnc2005@gmail.com>
2026-07-27 11:14:28 +0200

    Add: min-cost-to-connect-points - submission-8

---
+++ Data Structures & Algorithms/min-cost-to-connect-points/submission-8.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int minCostConnectPoints(int[][] points) {
+        int n = points.length;
+
+        // visited[i] = whether point i is already included in the MST
+        boolean[] visited = new boolean[n];
+
+        // dist[i] = minimum cost to connect point i to the current mST
+        int[] dist = new int[n];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+
+        int node = 0;
+        int totalCost = 0;
+        int edges = 0;
+        
+        // Prim's algorithm: add the cheapest edge connecting a new point each time
+        while (edges < n - 1) {
+            visited[node] = true;
+            int nextNode = -1;
+
+            // For every unvisited point i, compute cost to connect i from the current node
+            for (int i = 0; i < n; i++) {
+                if (!visited[i]) {
+                    int currDist = Math.abs(points[node][0] - points[i][0]) 
+                        + Math.abs(points[node][1] - points[i][1]);
+                    dist[i] = Math.min(dist[i], currDist);
+
+                    // Select the next point with minimum connection cost
+                    if (nextNode == - 1 || dist[i] < dist[nextNode]) {
+                        nextNode = i;
+                    }
+                }
+            }
+
+            // Add the cheapest connection to the MST
+            totalCost += dist[nextNode];
+            node = nextNode;
+            edges++;
+        }
+
+        return totalCost;
+    }
+}
