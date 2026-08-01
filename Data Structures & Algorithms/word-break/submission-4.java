52e510068161eaa7d3b276fa8b9995782abdf622
Cao Minh Nguyen <minhnc2005@gmail.com>
2026-08-01 17:49:01 +0200

    Add: word-break - submission-4

---
+++ Data Structures & Algorithms/word-break/submission-4.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        int n = s.length();
+
+        // dp[i] = true if s[0..i] can be segmented
+        boolean[] dp = new boolean[n];
+
+        for (int i = 0; i < n; i++) {
+            for (String word : wordDict) {
+                int len = word.length();
+
+                if (i + 1 < len) continue;
+
+                int start = i + 1 - len;
+
+                if (s.substring(start, i + 1).equals(word) && 
+                    (start == 0 || dp[start - 1])) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+
+        return dp[n-1];
+    }
+
+    // Time: O(n^2)
+    // Space: O(n)
+}
