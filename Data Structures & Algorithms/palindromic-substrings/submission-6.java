9ccdc1c7f71c7f8314e7e29d9134d30ac94e4b6d
Cao Minh Nguyen <minhnc2005@gmail.com>
2026-08-01 06:39:51 +0200

    Add: palindromic-substrings - submission-6

---
+++ Data Structures & Algorithms/palindromic-substrings/submission-6.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int countSubstrings(String s) {
+        int n = s.length();
+
+        // dp[i][j] = true if the substring s[i..j] is a palindrome
+        boolean[][] dp = new boolean[n][n];
+        int count = 0;
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = i; j < n; j++) {
+                if (s.charAt(i) == s.charAt(j) && (j - i <= 2 || dp[i+1][j-1])) {
+                    dp[i][j] = true;
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+
+    // Time: O(n^2)
+    // Space: O(n^2)
+}
