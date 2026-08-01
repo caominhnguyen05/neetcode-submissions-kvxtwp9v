cfcb92f61d37c8889eaebfdf88b7015c753b1f7e
Cao Minh Nguyen <minhnc2005@gmail.com>
2026-08-01 06:47:44 +0200

    Add: palindromic-substrings - submission-7

---
+++ Data Structures & Algorithms/palindromic-substrings/submission-7.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int countSubstrings(String s) {
+        int n = s.length();
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            // Find palindromes with odd length
+            int left = i;
+            int right = i;
+
+            while (left >= 0 && right <= n - 1) {
+                if (s.charAt(left) == s.charAt(right)) {
+                    count++;
+                    left--;
+                    right++;
+                } else {
+                    break;
+                }
+            }
+
+            // Find palindromes with even length
+            left = i - 1;
+            right = i;
+
+            while (left >= 0 && right <= n - 1) {
+                if (s.charAt(left) == s.charAt(right)) {
+                    count++;
+                    left--;
+                    right++;
+                } else {
+                    break;
+                }
+            }
+        }
+
+        return count;
+    }
+}
