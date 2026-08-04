eb813d18abacf85ad8863f30093be8356019288b
Cao Minh Nguyen <minhnc2005@gmail.com>
2026-08-04 04:58:17 +0200

    Add: count-paths - submission-5

---
+++ Data Structures & Algorithms/count-paths/submission-5.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int uniquePaths(int m, int n) {  
+        // dp[j] = number of possible unique paths to go to cell j in the current row
+        int[] dp = new int[n];
+
+        for (int i = 0; i < m; i++) {
+            int[] temp = new int[n];
+            for (int j = 0; j < n; j++) {
+                temp[j] = dp[j];
+            }
+
+            for (int j = 0; j < n; j++) {
+                if (i == 0 || j == 0) {
+                    dp[j] = 1;
+                } else {
+                    dp[j] += dp[j-1];
+                }
+            }
+        }
+
+        return dp[n-1];
+    }
+}
