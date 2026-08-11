d0055111065f3920269dee97a0b45c8897fb7095
Cao Minh Nguyen <minhnc2005@gmail.com>
2026-08-11 15:56:26 +0700

    Add: pow-x-n - submission-3

---
+++ Data Structures & Algorithms/pow-x-n/submission-3.java
@@ -0,0 +1,21 @@
+class Solution {
+    public double myPow(double x, int n) {
+        return n < 0 ? 1 / positivePow(x, -n) : positivePow(x, n);
+    }
+
+    private double positivePow(double x, int n) {
+        if (n == 0) return 1;
+
+        double half = positivePow(x, n / 2);
+
+        if (n % 2 == 0) {
+            return half * half;
+        }
+
+        return half * half * x;
+    }
+
+    // Binary exponentiation
+    // Time: O(log n)
+    // Space: O(log n)
+}
